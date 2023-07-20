010eb3d66da6b898af24a229db3252c46899364c
prod server <prod@notrealmail.ru>
2023-07-21 01:05:26 +0300

    main structure done*

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import logging
+import typing
 
 from aiogram import Bot, Dispatcher, types
 from aiogram.contrib.middlewares.logging import LoggingMiddleware
+from aiogram.dispatcher.dispatcher import DEFAULT_RATE_LIMIT
+from aiogram.dispatcher.storage import BaseStorage
 from aiogram.utils import executor
 
-from src.tg_keyboards.Inline import start_menu, servers_menu, profile_menu
-from src.tg_commands.MainMenu import cmd_start, cmd_servers, cmd_profile
+from src.tg_keyboards.InlineAllServersCallback import prc_allservers_menu, prc_return_to_allservers_menu
+from src.tg_keyboards.InlineMyServersCallback import prc_myservers_menu
+from src.tg_commands.MainMenu import cmd_start, cmd_allservers, cmd_myservers, cmd_profile
+from src.extensions.TGTimeoutCheck import Timeout
 import configuration
 
 
@@ -13,54 +18,31 @@ bot = Bot(token=configuration.telegram_token)
 dp = Dispatcher(bot)
 dp.middleware.setup(LoggingMiddleware())
 logging.basicConfig(level=logging.INFO)
+Timeout.bot = bot # Костыль для проброски bot xD
 
 for message_command in [
     cmd_start(),
-    cmd_servers(),
+    cmd_allservers(),
+    cmd_myservers(),
     cmd_profile(),
 ]:
     dp.register_message_handler(
-        callback=message_command[1],
-        commands=message_command[0]
+        callback=message_command["callback"],
+        commands=message_command["commands"]
     )
 
 
-# Обработчик нажатия на кнопки в старт
-@dp.callback_query_handler(lambda c: c.data in ['servers', 'profile', 'back_to_start'])
-async def process_callback_button(callback_query: types.CallbackQuery):
-    # Отправляем ответное сообщение согласно нажатой кнопке
-    if callback_query.data == 'servers':
-        await callback_query.message.edit_text("Выберите сервер:", reply_markup=servers_menu)
-    elif callback_query.data == 'profile':
-        await callback_query.message.edit_text("Выберите пункт меню профиля:", reply_markup=profile_menu)
-    elif callback_query.data == 'back_to_start':
-        await callback_query.message.edit_text("Вернулись в стартовое меню.", reply_markup=start_menu)
-
-    # Отмечаем обработанным callback
-    await callback_query.answer()
-
-
-# Обработчик нажатия на кнопки в меню серверов
-@dp.callback_query_handler(lambda c: c.data in ['server_1', 'server_2'])
-async def process_servers_menu(callback_query: types.CallbackQuery):
-    # Отправляем ответное сообщение согласно выбранному серверу
-    server_name = "Сервер 1" if callback_query.data == 'server_1' else "Сервер 2"
-    await callback_query.message.edit_text(f"Выбран сервер: {server_name}", reply_markup=start_menu)
-
-    # Отмечаем обработанным callback
-    await callback_query.answer()
-
-
-# Обработчик нажатия на кнопки в меню профиля
-@dp.callback_query_handler(lambda c: c.data in ['my_profile', 'settings'])
-async def process_profile_menu(callback_query: types.CallbackQuery):
-    # Отправляем ответное сообщение согласно выбранному пункту профиля
-    menu_item = "Мой профиль" if callback_query.data == 'my_profile' else "Настройки"
-    await callback_query.message.edit_text(f"Выбран пункт меню профиля: {menu_item}", reply_markup=start_menu)
-
-    # Отмечаем обработанным callback
-    await callback_query.answer()
-
+for callback_query_handler in [
+    # InlineAllServersCallback
+    prc_allservers_menu(),
+    prc_return_to_allservers_menu(),
+    # InlineMyServersCallback
+    prc_myservers_menu(),
+]:
+    dp.register_callback_query_handler(
+        callback_query_handler["callback"],
+        callback_query_handler["filter"]
+    )
 
 # Запуск бота
 if __name__ == '__main__':
