3b5113e87be8f7ce9972164bbbc4e8d027b3849d
prod server <prod@notrealmail.ru>
2023-07-26 00:05:14 +0300

    structure changes

--- src/tg_keyboards/InlineAllServersCallback.py
+++ src/tg_keyboards/InlineAllServersCallback.py
@@ -11,33 +11,51 @@ if __name__ != "__main__":
     from ..extensions.DockerWorkerExtension import *
 
 
-# Обработчик нажатия на кнопки "factorio", "astroneers"
+# Обработчик нажатия на кнопку "factorio" после выбора игры, также обрабатываю свои кнопки.
 def prc_allservers_menu_0():
-    condition = lambda c: c.data in ['factorio_new', 'astroneer_new']
-    async def process(callback_query: types.CallbackQuery):        
-        Timeout.update(callback_query.message.chat.id, callback_query.message.message_id, 5*60)
-        game_selected = "factorio" if "factorio_new" in callback_query.data else "astroneer"
+    condition = lambda c: c.data in ["factorio_new", "factorio_new:1.1.80", "factorio_new:1.1.87"]
+    async def process(callback_query: types.CallbackQuery):
+        Timeout.update(callback_query.message.chat.id, callback_query.message.message_id, 5 * 60)
         process_menu = InlineKeyboardMarkup(row_width=1)
-        process_menu.add( # TODO выбор версии после new_container!!!!!!!!
-            InlineKeyboardButton("Создать контейнер сервера", callback_data=f"new_container:{game_selected}"),
-            InlineKeyboardButton("Вернуться к выбору", callback_data="return_to_allservers"),
-        )
+        add_btn = lambda *b: process_menu.add(b)
+        if ":" not in callback_query.data:
+            msg_text = f"Выберите нужную версию игры:"
+            add_btn(*[InlineKeyboardButton(
+                f"factorio {version}",
+                callback_data=f"factorio_new:{version}"
+            ) for version in ["1.1.80", "1.1.87"]])
+        else: # version selected
+            version = callback_query.data.split(":")[1]
+            msg_text = f"Вы выбрали версию factorio: {version}"
+            add_btn(
+                InlineKeyboardButton(
+                    "Создать контейнер factorio",
+                    callback_data=f"new_container;factorio:1.1.87"
+                ), 
+                InlineKeyboardButton( # Не уверен в надобности данной кнопки
+                    "Вернуться к выбору версии",
+                    callback_data="factorio_new"
+                )
+            )
+        add_btn(InlineKeyboardButton(
+            "Вернуться к выбору игры",
+            callback_data="return_to_allservers"
+        ))
         await callback_query.message.edit_text(
-            f"Выбрана игра: {game_selected}", # TODO 
+            text=msg_text,
             reply_markup=process_menu
         )
         await callback_query.answer()
     return {"filter": condition, "callback": process}
 
 
-# Обработчик нажатия на кнопку "закрыть меню"
+# Обработчик нажатия на кнопку "astroneer" после выбора игры
 def prc_allservers_menu_1():
-    condition = lambda c: c.data == "close_menu"
-    async def process(callback_query: types.CallbackQuery):
-        Timeout.remove(callback_query.message.chat.id, callback_query.message.message_id)
-        await callback_query.message.delete()
-        await callback_query.answer()
+    condition = lambda c: c.data == "astroneer_new"
+    async def process(callback_query: types.CallbackQuery):  
+        ...
     return {"filter": condition, "callback": process}
+
 
 
 # Обработчик нажатия на кнопку "Создать контейнер сервера" после выбора игры
@@ -45,11 +63,12 @@ def prc_new_container_menu():
     condition = lambda c: "new_container" in c.data
     async def process(callback_query: types.CallbackQuery):
         Timeout.remove(callback_query.message.chat.id, callback_query.message.message_id, 5*60)
-        game_selected = "factorio" if "factorio" in callback_query.data else "astroneer"
-        import time # temp random =)
-        container_id = create_docker_container(f"factorio_test_{time.time()}", "factorio:1.1.87", 8672, 8672)
-        if container_id: # TODO выбор версии после new_container!!!!!!!!
-            msg =  f"Создан контейнер: {game_selected}!\n" \
+        port = 8672 # TODO сделать нормальное выделение портов
+        image_name = callback_query.split(";")[1]
+        container_name = f"{image_name}:{port}"
+        container_id = create_docker_container(container_name, image_name, port, port)
+        if container_id:
+            msg =  f"Создан контейнер: {container_name}!\n" \
                    f"id: {container_id}\n" \
                    f"Продолжить настройку: /myservers"
             try:
@@ -68,6 +87,16 @@ def prc_new_container_menu():
         await callback_query.message.edit_text(
             msg, reply_markup=None
         )
+        await callback_query.answer()
+    return {"filter": condition, "callback": process}
+
+
+# Обработчик нажатия на кнопку "закрыть меню"
+def prc_close_menu():
+    condition = lambda c: c.data == "close_menu"
+    async def process(callback_query: types.CallbackQuery):
+        Timeout.remove(callback_query.message.chat.id, callback_query.message.message_id)
+        await callback_query.message.delete()
         await callback_query.answer()
     return {"filter": condition, "callback": process}
 
@@ -95,6 +124,7 @@ allservers_handlers = [
     prc_allservers_menu_0,
     prc_allservers_menu_1,
     prc_new_container_menu,
+    prc_close_menu,
     prc_return_to_allservers_menu
 ]
 
